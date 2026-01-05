af8f35386269ed21cbbde92ebf94464d01c74f13
MYousuf-Codes <yousufhere.dev@gmail.com>
2026-01-05 08:59:25 +0500

    Optional params are used

--- query_params.py
+++ query_params.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from typing import Optional
+
 
 app = FastAPI()
 
@@ -7,6 +9,13 @@ app = FastAPI()
 @app.get("/greet/{name}") 
 async def greet_user(name: str, age: int) -> dict:
     return {"greeting" : f"Hello, {name}! You are {age} years old."}
+
+## Using the Optional Type for Query Parameters
+@app.get("/items/")
+async def read_item(item_name : Optional[str] = "New Item", item_id : int = 0) -> dict:
+    return {"item_id" : item_id, "item_name" : item_name}
+
+
 
 if __name__ == "__main__":
     import uvicorn
